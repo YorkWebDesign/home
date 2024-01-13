739b54234f71ab73d82674f1222130f820b7432c
Michael <michaelzaslavskytmpl@gmail.com>
2024-01-13 17:35:14 -0500

    email functionality

--- scripts.js
+++ scripts.js
@@ -1,4 +1,11 @@
 /*
-
-push test
 */
+
+function OpenAndFillEmailClient(){
+    const subjectValue = document.getElementById("subjectInput").value;
+    const nameValue = document.getElementById("nameInput").value;
+    const companyValue = document.getElementById("companyInput").value;
+    const messageValue = document.getElementById("messageInput").value;
+    const mailIntroduction = "Hi, my name is " + nameValue + " from " + companyValue + ".";
+    window.open("mailto:m.zaslavsky.ca@gmail.com?cc="+"&subject="+subjectValue+"&body="+mailIntroduction+" " + messageValue);
+}
